13a8f9ac69089493583b11d623896855cfd5e7a5
Vinay Mavi <vinaymavi@gmail.com>
2020-07-31 17:50:52 +0530

    npm package published

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# package-commons
-My Junior commons shared library
+# Commons
+
+Project common library shared among the projects
+
+## How to use it?
+
+Import and check how to use it.
+
+
+```
+import Gcp from "@myjunior/gcp-appengine";
+const keyFile = "service-account-key.json"
+const gcpProjectId = "project-xyz";
+const gcp = new Gcp(keyFile);
+
+// create AppEngine Application
+gcp.createInstance(gcpProjectId).then((resp)=>{
+  console.log(resp);
+}).catch(err=>{
+  console.log(err);
+});
+
+// Get AppEngine Application
+gcp.getInstance(gcpProjectId).then((resp)=>{
+  console.log(resp);
+}).catch(err=>{
+  console.log(err);
+});
+
+
+```
+
+## NPM publish 
+
+* create a access token with read+publish permission
+* set access token as environment variable `export NPM_TOKEN="000-000-0000-00000"`
+* run `npm run push` command 
+
---
+++ index.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+export function createKeyFile(serviceAccountKeyBase64: string) {
+  const buff = new Buffer(serviceAccountKeyBase64, "base64");
+  const fileName = `/tmp/${uuidv4()}`;
+  fs.writeFileSync(fileName, buff.toString("ascii"));
+  return fileName;
+}
+
+export function errorData(error: any) {
+  let data = {};
+  if (error && error.response && error.response.data) {
+    data = error.response.data;
+  } else {
+    console.log(`Error data not defined`);
+  }
+  return data;
+}
+
+export async function sendSuccessResponse(
+  jwtToken: string,
+  taskToken: string,
+  output: any
+): Promise<any> {
+  if (process.env.MYJUNIOUR_TASK_SUCCESS_URL) {
+    await axios.post(process.env.MYJUNIOUR_TASK_SUCCESS_URL, output, {
+      headers: { Authorization: `Bearer ${jwtToken}` },
+    });
+    console.log("Success status sent");
+  } else {
+    console.warn("Success end point not defined");
+  }
+}
+export async function sendErrorResponse(
+  jwtToken: string,
+  taskToken: string,
+  output: any
+): Promise<any> {
+  if (process.env.MYJUNIOUR_TASK_ERROR_URL) {
+    await axios.post(process.env.MYJUNIOUR_TASK_ERROR_URL, output, {
+      headers: { Authorization: `Bearer ${jwtToken}` },
+    });
+    console.log("Error status sent");
+  } else {
+    console.warn("Error end point not defined");
+  }
+}
+
+export function hasPermission(
+  permissions: Array<string>,
+  checkPermission: string
+): boolean {
+  return permissions.includes(checkPermission);
+}
